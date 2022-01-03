630e93f0c055ec77d623ae1b3a003e29eebd482f
Alejandro <amogalla@iesrafaelalberti.es>
2022-01-03 20:10:36 +0100

    Añadidos los fragmentos

--- app/src/androidTest/java/com/example/taquillamirandilla/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/taquillamirandilla/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.taquillamirandilla", appContext.packageName)
+        assertEquals("com.example.amazonia", appContext.packageName)
     }
 }
