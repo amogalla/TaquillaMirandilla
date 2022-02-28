9775ce8cb1da6ee712736512394128eeafe36f2e
Alejandro <amogalla@iesrafaelalberti.es>
2022-02-28 12:43:02 +0100

    Añadido inicio para RecyclerView.

---
+++ app/src/main/java/com/example/taquillamirandilla/partido/PartidoAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.taquillamirandilla.partido
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.taquillamirandilla.TextItemViewHolder
+
+/*class PartidoAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    //var data = listOf<
+
+}*/
